a48218fcfa4e142cc1151b17093126fa09c3939a
cesargua <cguajard@ucsd.edu>
2023-04-25 19:07:31 -0700

    databases

--- database/index.js
+++ database/index.js
@@ -3,14 +3,46 @@ mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  id: Number,
+  repo_name: String,
+  description: String,
+  owner_name: String,
+  owner_url: String,
+  repo_url: String,
+  forks: Number,
+  watchers: Number,
+  stars:Number,
+  totalCount: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (data) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  const repo = new Repo(data);
+  console.log(repo)
+
+  //add a conditional where only insert to db if its not already in there
+  repo.save((err)=>{
+    if(err) console.error(err)
+    else {
+      console.log('Inserted repo to db successfully!')
+    }
+  })
 }
 
-module.exports.save = save;
+let retrieve = (cb)=> {
+  // Repo.find
+  Repo.find({}, (err, data)=>{
+    if(err){
+      cb(err)
+    } else {
+      cb(null,data);
+    }
+  })
+
+}
+
+module.exports = {save, retrieve};
